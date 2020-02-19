181a375aaa4805243dfd3107f521b04c101b94cd
darksystem <diogo.rey@hotmail.com>
2020-02-19 15:27:31 +0000

    Refactor - Extract code to an external file and added chart.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -139,6 +139,7 @@ module.exports = function(grunt) {
                     "packages/node_modules/@node-red/editor-client/src/js/font-awesome.js",
                     "packages/node_modules/@node-red/editor-client/src/js/history.js",
                     "packages/node_modules/@node-red/editor-client/src/js/validators.js",
+                    "packages/node_modules/@node-red/editor-client/src/js/monitoring/darkmagic.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/utils.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/common/editableList.js",
                     "packages/node_modules/@node-red/editor-client/src/js/ui/common/treeList.js",
